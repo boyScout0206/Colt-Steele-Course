d2e92e7e138a3d3fe7b46e2dff0f9feddb2a7131
boyScout0206 <kurt.glen@gmail.com>
2018-05-13 00:43:11 -0700

    Finally uploaded my course files so far.

---
+++ Javascript small exercises/annoy.js
@@ -0,0 +1,5 @@
+var answer = prompt("Are we there yet?");
+while(answer != "yes") {
+	 answer = prompt("Are we there yet?");
+}
+alert("yay!!!");
---
+++ Javascript small exercises/myAgeScript.js
@@ -0,0 +1,5 @@
+var birthYear = prompt("What year were you born?");
+var currentYear = prompt("what year is it?");
+var age = currentYear - birthYear;
+alert("You are " + age + " years old");
+console.log("User's age is: " + age + ".");
